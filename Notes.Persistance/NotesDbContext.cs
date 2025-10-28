5699251ab3146d346f6bdc71981894710ee37900
smazhserg@gmail.com <smazhserg@gmail.com>
2025-10-28 15:29:55 +0300

    Added DbContext

---
+++ Notes.Persistance/NotesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistance.EntityTypeConfigurations;
+
+namespace Notes.Persistance
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
