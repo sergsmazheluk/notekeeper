f8d33f1180e0d1bea45c1cc5b07fd00a53069cea
smazhserg@gmail.com <smazhserg@gmail.com>
2025-10-29 22:41:14 +0300

    Added Handler of query for getting list of Notes

---
+++ Notes.Application/Notes/Commands/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+
+namespace Notes.Application.Notes.Commands.Queries.GetNoteList
+{
+    public class GetNoteListQueryHandler : IRequestHandler<GetNoteListQuery, NoteListVm>
+    {
+        private readonly INotesDbContext dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandler(INotesDbContext notesDbContext, IMapper mapper)
+        {
+            dbContext = notesDbContext;
+            _mapper = mapper;
+        }
+        public async Task<NoteListVm> Handle(GetNoteListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var notesQuery = await dbContext.Notes
+                .Where(note => note.UserId == request.UserId)
+                .ProjectTo<NoteLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new NoteListVm { Notes = notesQuery };
+        }
+    }
+}
