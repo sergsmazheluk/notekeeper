59d1277a4bdbff6331bcfc4bd19f0863eeac283f
smazhserg@gmail.com <smazhserg@gmail.com>
2025-10-29 23:57:03 +0300

    Добавление контекста БД через DI и его регистрация

---
+++ Notes.Persistance/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+
+namespace Notes.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DbConnection") ??
+                throw new InvalidOperationException("Connection string 'DbConnection' " +
+                "not found.");
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddScoped<INotesDbContext, NotesDbContext>();
+
+            return services;
+        }
+    }
+}
